6cdff50b2efe665bedb6def600ad1e3aa0dea8d8
robert-rada <robert_gabriel.rada@stud.acs.upb.ro>
2017-07-27 23:29:01 +0300

    + data

--- main.cpp
+++ main.cpp
@@ -12,6 +12,14 @@ using namespace std;
 string project_name;
 vector<string> files;
 unsigned char separator[12] = {6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6};
+vector<unsigned int> data_location;
+unsigned int current_address = 0;
+
+void waitKey()
+{
+    int x;
+    cin >> x;
+}
 
 void getProjectName(char *argv[])
 {
@@ -54,6 +62,7 @@ void extractFiles()
     else
     {
         perror ("");
+        waitKey();
         exit(1);
     }
 }
@@ -64,6 +73,7 @@ void copyFile(const char *file_name, FILE *output_file)
     if (file == NULL)
     {
         cerr << "ERROR: Could not open loader.gba\n";
+        waitKey();
         exit(2);
     }
 
@@ -78,6 +88,7 @@ void copyFile(const char *file_name, FILE *output_file)
     if (buffer == NULL)
     {
         cerr << "Memory error\n";
+        waitKey();
         exit(10);
     }
 
@@ -87,12 +98,65 @@ void copyFile(const char *file_name, FILE *output_file)
     fwrite(buffer, 1, file_size, output_file);
     fwrite(separator, 1, 12, output_file);
 
+    current_address += file_size + 12;
+
     free(buffer);
 }
 
 void copyData(FILE *output_file)
 {
+    unsigned int mem_loc = current_address;
+    unsigned char zero = 0;
 
+    for (int i = 0; i < 4 * files.size(); i++)
+    {
+        fwrite(&zero, 1, 1, output_file);
+        current_address++;
+    }
+
+    for (int i = 0; i < files.size(); i++)
+    {
+        data_location.push_back(current_address + 0x08000000);
+
+        cerr << files[i] << '\n';
+
+        string file_name =  "data\\" + files[i];
+
+        FILE *file = fopen(file_name.c_str(), "rb");
+        if (file == NULL)
+        {
+            cerr << "ERROR: Could not open " << files[i] << '\n';
+            waitKey();
+            exit(2);
+        }
+
+        long file_size;
+        unsigned char *buffer;
+
+        fseek(file, 0, SEEK_END);
+        file_size = ftell(file);
+        rewind(file);
+
+        buffer = (unsigned char*)malloc(file_size);
+        if (buffer == NULL)
+        {
+            cerr << "Memory error\n";
+            waitKey();
+            exit(10);
+        }
+
+        fread(buffer, 1, file_size, file);
+        fclose(file);
+
+        fwrite(buffer, 1, file_size, output_file);
+        current_address += file_size;
+
+        free(buffer);
+    }
+
+    fseek(output_file, mem_loc, SEEK_SET);
+    for (int i = 0; i < data_location.size(); i++)
+        fwrite(&data_location[i], 4, 1, output_file);
 }
 
 int main(int argc, char *argv[])
@@ -107,6 +171,8 @@ int main(int argc, char *argv[])
     copyFile("loader.gba", output_file);
     string bin_name = project_name + ".bin";
     copyFile(bin_name.c_str(), output_file);
+
+    copyData(output_file);
 
     fclose(output_file);
 
